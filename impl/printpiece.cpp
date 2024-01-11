ddf6baa3f28b39d9e9170b33cb6fadd109e383bd
liwei <liwei1@cxsw3d.com>
2024-01-11 15:39:39 +0800

    add:[nestplacer]增加多边形扫掠轮廓及碰撞检测接口
    Change-Id: Idbe9da38969a0fcceaa08a3b0660699d1f40e83f

---
+++ impl/printpiece.cpp
@@ -0,0 +1,15 @@
+#include "nestplacer/printpiece.h"
+
+namespace nestplacer {
+    PR_Polygon sweepAreaProfile(const PR_Polygon& station, const PR_Polygon& orbit, const trimesh::vec3& mp)
+    {
+        PR_Polygon poly;
+
+        return poly;
+    }
+
+    void collisionCheck(const std::vector<PR_Polygon>& polys, std::vector<PR_RESULT>& results)
+    {
+
+    }
+}
---
+++ nestplacer/printpiece.h
@@ -0,0 +1,29 @@
+#ifndef PRINT_PIECE_H
+#define PRINT_PIECE_H
+#include "nestplacer/export.h"
+#include "ccglobal/tracer.h"
+
+#include "trimesh2/Box.h"
+#include <vector>
+
+namespace nestplacer {
+
+    typedef std::vector<trimesh::vec3> PR_Polygon;
+
+    NESTPLACER_API PR_Polygon sweepAreaProfile(const PR_Polygon& station, const PR_Polygon& orbit, const trimesh::vec3& mp);
+
+    enum class ContactState :int {
+        INTERSECT = 0,
+        TANGENT,
+        SEPARATE
+    };
+
+    struct PR_RESULT {
+        int first;
+        int second;
+        ContactState state;
+    };
+
+    NESTPLACER_API void collisionCheck(const std::vector<PR_Polygon>& polys, std::vector<PR_RESULT>& results);
+}
+#endif //PRINT_PIECE_H
